2d7906018ec34b62950a2f3dfba6a338870fd567
Your Name Here <you@yourdomain.com>
2016-03-19 14:23:53 +0900

    Initial Commit

---
+++ SampleUIRefreshControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleUIRefreshControl
+//
+//  Created by cat-07 on 2016/03/19.
+//  Copyright © 2016年 cat-07. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
